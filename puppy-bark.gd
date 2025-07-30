b8711112714d8a8250a630afaeb4d1be6893e999
Isabelle <121070808+nik0-dev@users.noreply.github.com>
2025-07-29 21:42:13 -0500

    Initial commit

---
+++ puppy-bark.gd
@@ -0,0 +1,29 @@
+@tool
+extends EditorPlugin
+
+const SOUND : AudioStreamWAV = preload("res://addons/puppy-bark/puppy-bark.wav")
+const MAX_PLAYERS : int = 8
+var audio_players : Array[AudioStreamPlayer] = []
+
+func _enter_tree() -> void:
+	for i in MAX_PLAYERS:
+		var ap = AudioStreamPlayer.new()
+		ap.stream = SOUND
+		get_tree().root.add_child(ap)
+		audio_players.append(ap)
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var available : Array[AudioStreamPlayer] = audio_players.filter(
+			func(ap: AudioStreamPlayer): 
+				return !ap.playing
+		)
+		
+		if available.size() > 0:
+			var ap = available.front()
+			ap.pitch_scale = randf_range(1, 1.2)
+			ap.play()
+
+func _exit_tree() -> void:
+	for ap in audio_players:
+		ap.queue_free()
